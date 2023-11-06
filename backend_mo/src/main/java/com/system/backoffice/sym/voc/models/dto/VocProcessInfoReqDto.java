a7c28dde7b6d286fc642b4477817b96b8baeab73
sohok <sohok@host.docker.internal>
2023-11-06 14:17:19 +0900

    누락분

---
+++ backend_mo/src/main/java/com/system/backoffice/sym/voc/models/dto/VocProcessInfoReqDto.java
@@ -0,0 +1,28 @@
+package com.system.backoffice.sym.voc.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class VocProcessInfoReqDto {
+
+	private String mode;
+	private String vocSeq;
+	private String vocProcessSeq;
+	private String vocProcessUserid;
+	private String vocProcessVisitedDay;
+	private String vocProcessVisitedTime;
+	private String vocProcessEndDate;
+	private String vocProcess;
+	private String productCode;
+	private String vocProcessDc;
+	private String userId;
+	
+}
