d251aa799d5dff463d7b5e7f5c6e7cdb24e80024
sohoki@daum.net <sohoki@daum.net>
2024-07-20 17:52:22 +0900

    snmp receive

--- backend_mo/src/main/java/com/system/backoffice/sym/svr/web/ServerInfoManageController.java
+++ backend_mo/src/main/java/com/system/backoffice/sym/svr/web/ServerInfoManageController.java
@@ -22,6 +22,7 @@ import org.snmp4j.Target;
 import org.snmp4j.fluent.SnmpBuilder;
 import org.snmp4j.fluent.SnmpCompletableFuture;
 import org.snmp4j.fluent.TargetBuilder;
+import org.snmp4j.mp.SnmpConstants;
 import org.snmp4j.smi.Address;
 import org.snmp4j.smi.GenericAddress;
 import org.snmp4j.smi.Integer32;
@@ -505,7 +506,7 @@ public class ServerInfoManageController {
 					}else if (info.get().getOidResultType().equals("IPCC_SNMP_VALUE_GUBUN_3")) {
 						pdu.setType(PDU.GETNEXT);
 					}else {
-						pdu.setType(PDU.REPORT);
+						pdu.setType(PDU.GETBULK);
 					}
 					log.info("getOidResultType:"  + info.get().getOidResultType());
 					pdu.setRequestID(new Integer32(1));
@@ -531,26 +532,46 @@ public class ServerInfoManageController {
 					snmp.close();
 				}else {
 					
-					
 					Snmp snmp = snmpBuilder.udp().v3().usm().threads(2).build();
 					snmp.listen();
 					Address targetAddress = GenericAddress.parse("udp:"+info.get().getServerIp()+"/161");
 					byte[] targetEngineID = snmp.discoverAuthoritativeEngineID(targetAddress, 1000);
 					if (targetEngineID != null) {
 						TargetBuilder<?> targetBuilder = snmpBuilder.target(targetAddress);
+						
+						
+						
 						Target<?> userTarget = targetBuilder
 								.user(info.get().getServiceSnmpId(), targetEngineID)
-								.auth(TargetBuilder.AuthProtocol.hmac192sha256).authPassphrase(info.get().getServiceSnmpPassword())
-								.priv(TargetBuilder.PrivProtocol.aes128).privPassphrase(info.get().getServiceSnmpPrivacy())
+								.auth(TargetBuilder.AuthProtocol.sha1).authPassphrase(info.get().getServiceSnmpPassword())
+								.priv(TargetBuilder.PrivProtocol.aes128).privPassphrase("")
 								.done()
 								.timeout(500).retries(1)
 								.build();
-			
-						PDU pdu = targetBuilder.pdu().type(PDU.GETNEXT).oids(info.get().getOidNumber()).contextName(info.get().getServiceSnmpCommunityName()).build();
+						
+						userTarget.setVersion(SnmpConstants.version3);
+						
+						log.info("===============   " + info.get().getServiceSnmpId());
+						log.info("===============   " + info.get().getServiceSnmpPassword());
+						log.info("===============   " + info.get().getServiceSnmpPrivacy());
+						
+						int type = 0;
+						if (info.get().getOidResultType().equals("IPCC_SNMP_VALUE_GUBUN_1")) {
+							type = PDU.GET;
+						}else if (info.get().getOidResultType().equals("IPCC_SNMP_VALUE_GUBUN_3")) {
+							type = PDU.GETNEXT;
+						}else {
+							type = PDU.REPORT;
+						}
+						
+						
+						PDU pdu = targetBuilder.pdu().type(type).oids(info.get().getOidNumber()).contextName(info.get().getServiceSnmpCommunityName()).build();
+						log.info("===============   2" + type);
 						SnmpCompletableFuture snmpRequestFuture = SnmpCompletableFuture.send(snmp, userTarget, pdu);
+						log.info("===============   3");
 						try {
 							List<VariableBinding> vbs = snmpRequestFuture.get().getAll();
-				
+							
 							System.out.println("Received: " + snmpRequestFuture.getResponseEvent().getResponse());
 							System.out.println("Payload:  " + vbs);
 							if (snmpRequestFuture.getResponseEvent().getResponse().getErrorIndex() ==0 ) {
@@ -563,6 +584,7 @@ public class ServerInfoManageController {
 							}
 						} catch (ExecutionException | InterruptedException ex) {
 							if (ex.getCause() != null) {
+								log.info("===============  ex.getCause()");
 								System.err.println(ex.getCause().getMessage());
 							} else {
 								System.err.println("Request failed: "+ex.getMessage());
@@ -644,7 +666,10 @@ public class ServerInfoManageController {
 		session = new JSch().getSession(userId, host, Integer.parseInt(port));
 		session.setPassword(pwd);
 		session.setX11Host(host);
-		session.setX11Port( Integer.parseInt(port));
+		int xport=0;
+		session.setX11Port(xport);
+		
+	
 		session.setConfig("StrictHostKeyChecking", "no");
 		log.info("Connect==================");
 		session.connect();
